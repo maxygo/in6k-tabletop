e4620a3f7a0dd63af8e78e2c3f3d8f4db5ae4b03
Maxim Golovenskiy <maximg@interlink-ua.com>
2015-07-17 18:30:09 +0300

    remove compiled js files by TS and add them all in .gitignore

--- src/webapp/app/bootstrap.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../../typings/tsd.d.ts" />
-var angular2_1 = require('angular2/angular2');
-var app_1 = require('./pages/app');
-angular2_1.bootstrap(app_1.App, []);
-//# sourceMappingURL=bootstrap.js.map
